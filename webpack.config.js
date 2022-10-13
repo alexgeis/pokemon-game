e0dca119932e6fe02d94132dca0801baa428adbf
Alex Geis <siegxela@gmail.com>
2022-10-13 07:44:04 -0600

    staging

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
 	entry: "./src/index.js",
+	devtool: "inline-source-map",
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Mon Game",
+		}),
+	],
 	output: {
 		filename: "bundle.js",
 		path: path.resolve(__dirname, "dist"),
+		clean: true,
 	},
 	module: {
 		rules: [
